3fa021b28a5e98208a0a412540610f2a7064fa73
Chanty Herrmann <chanty@Chantys-MacBook-Air.local>
2019-08-27 21:31:20 +0200

    not done yet

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,8 +1,7 @@
-
 const express = require('express');
-const hbs     = require('hbs');
-const app     = express();
-const path    = require('path');
+const hbs = require('hbs');
+const app = express();
+const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
 
@@ -16,6 +15,26 @@ app.get('/', (req, res, next) => {
   res.render('index');
 });
 
+hbs.registerPartials(__dirname + '/views/beers.hbs');
+
+app.get('/beers', (req, res, next) => {
+
+  punkAPI.getBeers()
+    .then(beers => {
+      res.render('beers');
+    })
+    .catch(error => {
+      console.log(error)
+    })
+});
+
+app.get('/random-beers', (req, res, next) => {
+  res.render('random-beers');
+});
+
+// app.get('/gallery', (request, response, next) => {
+//   response.sendFile(__dirname + '/views/gallery.html');
+// });
 
 
 app.listen(3000);
